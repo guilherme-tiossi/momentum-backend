3976a4437fe07f53e917a4236eb5b634e676c60a
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2025-02-14 15:53:51 -0300

    feat(tests): added task crud tests

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+class TaskFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Task::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'parent_id' => null,
+            'title' => $this->faker->sentence(4),
+            'description' => $this->faker->paragraph(),
+            'date' => Carbon::now()->format('Y-m-d'),
+            'finished' => $this->faker->boolean(20),
+            'level' => 0,
+            'includes_weekend' => $this->faker->boolean(50),
+        ];
+    }
+
+    /**
+     * Indicate that the task is a subtask.
+     */
+    public function subtask(Task $parentTask)
+    {
+        return $this->state(function () use ($parentTask) {
+            return [
+                'parent_id' => $parentTask->id,
+                'level' => $parentTask->level + 1,
+            ];
+        });
+    }
+
+    /**
+     * Indicate that the task is finished.
+     */
+    public function finished()
+    {
+        return $this->state(fn() => ['finished' => true]);
+    }
+}
